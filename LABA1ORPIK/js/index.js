017b9060348c524a860baae035bff74e3d7c7952
github-VladOrpik <Orpik_VV_18@mf.grsu.by>
2021-03-01 22:16:01 +0300

    fix design

--- LABA1ORPIK/js/index.js
+++ LABA1ORPIK/js/index.js
@@ -2,4 +2,6 @@ var clicks = 0;
     function onClick() {
         clicks += 1;
         document.getElementById("clicks").innerHTML = clicks;
+        var x = document.getElementById("clicks");
+        x.style.fontSize = "50px";
     };
