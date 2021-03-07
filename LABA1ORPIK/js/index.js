e00aa61b6b759d1416cc154e2a6f2bc8a738b70a
github-VladOrpik <65450885+VladOrpik@users.noreply.github.com>
2021-03-07 12:32:38 +0300

    fix indentation

--- LABA1ORPIK/js/index.js
+++ LABA1ORPIK/js/index.js
@@ -1,7 +1,7 @@
 var clicks = 0;
-    function onClick() {
-        clicks += 1;
-        document.getElementById("clicks").innerHTML = clicks;
-        var x = document.getElementById("clicks");
-        x.style.fontSize = "50px";
+function onClick() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+    var x = document.getElementById("clicks");
+    x.style.fontSize = "50px";
     };
